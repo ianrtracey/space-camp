fb4310e08c3ca0467afe07eeea84b65f73f21550
Ian Tracey <ianrtracey@gmail.com>
2022-01-14 16:32:46 -0500

    done

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,39 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home () {
   return (
-    <div className="container">
-    <h1 className='title'>Space Camp</h1>
-<div className='solar-syst'>
-  <div className='sun'></div>
-  <div className='mercury'></div>
-  <div className='venus'></div>
-  <div className='earth'></div>
-  <div className='mars'></div>
-  <div className='jupiter'></div>
-  <div className='saturn'></div>
-  <div className='uranus'></div>
-  <div className='neptune'></div>
-  <div className='pluto'></div>
-  <div className='asteroids-belt'></div>
-</div>
-</div>
+    <>
+      <div className='container'>
+        <div className='marquee'>
+          <h2 className='logo'>Good Night Out</h2>
+          <p className='subtitle'>Presents</p>
+          <h1 className='title'>Space Camp</h1>
+        </div>
+        <div className='details'>
+          <div className='description'>
+            An extraterrestrial party in Brooklyn, NY
+          </div>
+          <div className=''>🗓&ensp;February 26th, 2022</div>
+          <div>📍&ensp;Eris</div>
+          <div>⏱&ensp;10pm - late</div>
+          <div className='button'>get tickets 👽</div>
+        </div>
+      </div>
+
+      <div className='solar-syst'>
+        <div className='sun'></div>
+        <div className='mercury'></div>
+        <div className='venus'></div>
+        <div className='earth'></div>
+        <div className='mars'></div>
+        <div className='jupiter'></div>
+        <div className='saturn'></div>
+        <div className='uranus'></div>
+        <div className='neptune'></div>
+        <div className='pluto'></div>
+        <div className='asteroids-belt'></div>
+      </div>
+    </>
   )
 }
