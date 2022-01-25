a6fa0fd83ca620317543d45191ff5e00c0cfa772
Ian Tracey <ianrtracey@gmail.com>
2022-01-25 01:49:13 -0500

    fixes flashing text

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,24 @@ function MyApp ({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link
+          rel='preload'
+          href='/fonts/Bangers-Regular.ttf'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/MrDafoe-Regular.ttf'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/Lato-Bold.ttf'
+          as='font'
+          crossOrigin=''
+        />
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0,width=100'
